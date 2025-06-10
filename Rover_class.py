31d0d2d729b5dc6eaba1d0052e2ecdd324eb5373
Bailey C. Hopkins <183045661+Bailey-C-Hopkins@users.noreply.github.com>
2025-06-10 11:26:51 -0600

    Create Rover_class.py
    Adds a rover class so the rover can be simulated

---
+++ Rover_class.py
@@ -0,0 +1,29 @@
+class Rover:
+    def __init__(self, soil_params, wheel_params, wheel_count=4):
+        self.wheels = []
+        for _ in range(wheel_count):
+            running_state = RunningStateParameters()
+            wheel = Wheel(soil_params, wheel_params, running_state)
+            self.wheels.append(wheel)
+
+    def get_total_forces(self):
+        total_F_W = 0
+        total_F_DP = 0
+        total_T = 0
+        per_wheel = []
+        for wheel in self.wheels:
+            forces = wheel.compute_forces()
+            total_F_W += forces['F_W']
+            total_F_DP += forces['F_DP']
+            total_T += forces['T']
+            per_wheel.append(forces)
+        return {
+            'Total_F_W': total_F_W,
+            'Total_F_DP': total_F_DP,
+            'Total_T': total_T,
+            'Per_Wheel': per_wheel
+        }
+
+    def update_wheel_states(self, omegas, theta_1s, theta_2s):
+        for i, wheel in enumerate(self.wheels):
+            wheel.update_running_state(omegas[i], theta_1s[i], theta_2s[i])
